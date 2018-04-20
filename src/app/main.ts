89643fff2dc88c929b5195395f41302b1d20c08a
Federico Meini <meini@mpib-berlin.mpg.de>
2018-04-20 17:23:24 +0200

    landscape only

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,6 +3,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 // RELEASE: disable console log
-console.log = function(){};
+// console.log = function(){};
 
 platformBrowserDynamic().bootstrapModule(AppModule);
