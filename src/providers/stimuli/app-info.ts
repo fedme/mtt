ebf43effd4035a2cade1bb330043cd328378465e
Federico Meini <meini@mpib-berlin.mpg.de>
2018-11-19 17:59:22 +0100

    Increase version number

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "5c42aff5";
     static nameLabel: string = "MTT";
-    static version: string = "2.8.0";
+    static version: string = "2.8.1";
 }
