cf3dd3f183df96eb6a182c40e3ff5576af26ecd7
Federico Meini <meini@mpib-berlin.mpg.de>
2018-12-06 11:38:34 +0100

    Updated failure page

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "5c42aff5";
     static nameLabel: string = "MTT";
-    static version: string = "2.9.0";
+    static version: string = "2.9.1";
 }
