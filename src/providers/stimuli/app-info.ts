6281ec480c1f32d48937df64796ff4ab051b042a
Federico Meini <federico.meini@gmail.com>
2019-05-16 17:47:01 +0200

    Update instructions

--- src/providers/stimuli/app-info.ts
+++ src/providers/stimuli/app-info.ts
@@ -1,5 +1,5 @@
 export abstract class AppInfo {
     static id: string = "5c42aff5";
     static nameLabel: string = "MTT";
-    static version: string = "2.10.1";
+    static version: string = "2.10.2";
 }
